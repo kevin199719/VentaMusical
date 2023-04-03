f2998b6b6660508c2a3035d11848efbcfaae0afc
ElJohens <frederickjohens100@gmail.com>
2023-04-02 23:29:50 -0600

    Se muestran las canciones disponibles en la base de datos a los clientes

---
+++ Models/SongModel.cs
@@ -0,0 +1,9 @@
+﻿namespace VentaMusical.Models
+{
+    public class SongModel
+    {
+        public int SongId { get; set; }
+        public string SongName { get; set; }
+        public string Description { get; set; }
+    }
+}
