519cd71e9c1e219208d381714e0f400b07b6c233
ElJohens <frederickjohens100@gmail.com>
2023-04-10 01:10:10 -0600

    Ultima actualizacion Frederick - Se corrige todo lo que no funcionaba en CRUD Albumes y se sincroniza con todo lo ultimo de la tienda y compra de canciones.

---
+++ Models/AlbumeEdit.cs
@@ -0,0 +1,15 @@
+﻿namespace VentaMusical.Models
+{
+    public class AlbumeEdit
+    {
+        public int AlbumeId { get; set; }
+
+        public int AuthorId { get; set; }
+
+        public string AlbumeName { get; set; } = null!;
+
+        public DateTime AlbumeYear { get; set; }
+
+        public bool AlbumeState { get; set; }
+    }
+}
