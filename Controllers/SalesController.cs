0a03d8cc0b9713b4a31edcffd499fee60b907fce
ElJohens <frederickjohens100@gmail.com>
2023-04-09 20:17:01 -0600

    FacturacionClientes

--- Controllers/SalesController.cs
+++ Controllers/SalesController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Policy;
 using VentaMusical.Data;
 using VentaMusical.Models;
+using VentaMusical.Models.Entities;
 
 namespace VentaMusical.Controllers
 {
@@ -13,10 +16,37 @@ namespace VentaMusical.Controllers
             return View();
         }
 
-        public JsonResult GetSongs()
+        public ActionResult Car()
         {
-            List<SongModel> songs = SalesBLL.getSongs();
+            return View();
+        }
+
+        public JsonResult GetSongs(string SongName, string AlbumeName, string AuthorName, string GenderDescription)
+        {
+            List<SongModel> songs = SalesBLL.getSongs(SongName,  AlbumeName,  AuthorName,  GenderDescription);
             return Json(songs);
+        }
+      
+        public int AddSong(int songId)
+        {
+            int userId = HttpContext.Session.GetInt32("_Id") ?? 0;
+            int invoiceHeaderId = SalesBLL.AddSongToInvoice(userId, songId);
+
+            return invoiceHeaderId;
+        }
+
+        public JsonResult getInvoiceSongs()
+        {
+            int userId = HttpContext.Session.GetInt32("_Id") ?? 0;
+            List<SongModel> songs = SalesBLL.getInvoiceSongs(userId);
+            return Json(songs);
+        }
+
+        public JsonResult getUserCard()
+        {
+            int userId = HttpContext.Session.GetInt32("_Id") ?? 0;
+            UserCard card = SalesBLL.getUserCard(userId);
+            return Json(card);
         }
     }
 }
